c26e3ba9088abe28935d1f1c9c37604e23033957
github username <harr.hughes@gmail.com>
2020-08-18 16:55:44 +0100

    controllers started

--- rails-server/app/models/request.rb
+++ rails-server/app/models/request.rb
@@ -1,4 +1,10 @@
 class Request < ApplicationRecord
   belongs_to :item
   belongs_to :order
+  
+  def self.make_requests(order, request_objects) # request objects => [{item_id: ~, amount: ~}, ...]
+    output = request_objects.map{ |r| Request.create(order: order, item: Item.find_by(id: r["item_id"].to_i), amount: r["amount"])}
+    return output
+  end
+
 end
